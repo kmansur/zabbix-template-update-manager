03cc51a794f9e456ba6dd2709e484eb34803a180
Karim Mansur <karim.mansur@outlook.com>
2026-09-23 16:26:11 -0300

    ux: normalize user-facing controller messages

--- actions/TemplateBatchPrepare.php
+++ actions/TemplateBatchPrepare.php
@@ -80,7 +80,7 @@ class TemplateBatchPrepare extends CController {
 		catch (Throwable $exception) {
 			error_log('[Zabbix Template Update Manager] Batch preparation shell failed: '.$exception->getMessage());
 			$data['error'] = _(
-				'Unable to rebuild the selected template set. No preparation or configuration import was attempted.'
+				'Unable to prepare the selected template set. No configuration import was attempted.'
 			);
 		}
 
