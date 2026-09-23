3977d447c53c845ed6aa692d66343fcaec1f5b60
Karim Mansur <karim.mansur@outlook.com>
2026-09-22 21:04:37 -0300

    refactor: use native page controls in template.rollback.php

--- views/template.rollback.php
+++ views/template.rollback.php
@@ -6,7 +6,11 @@ $backUrl = (new CUrl('zabbix.php'))
 
 $page = (new CHtmlPage())
 	->setTitle($data['title'])
-	->addItem(new CLink(_('Back to rollback backup history'), $backUrl));
+	->setControls(
+		(new CTag('nav', true,
+			(new CList())->addItem(new CLink(_('Back to rollback backup history'), $backUrl))
+		))->setAttribute('aria-label', _('Content controls'))
+	);
 
 if ($data['operation_error'] !== null) {
 	$page
